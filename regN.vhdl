dc99527ff500946146c0d88706ceabea75f8dba6
Maxime MULTARI <maxime.multari@neuf.fr>
2015-04-15 14:32:57 +0200

    Validation du schema et de la simulation du reg

--- regN.vhdl
+++ regN.vhdl
@@ -4,16 +4,16 @@ use IEEE.std_logic_arith.all;
 use IEEE.std_logic_unsigned.all;
 
 entity regN is --registre de N bits, où N est un paramètre générique
-	generic (N : natural := 16);
-	port (clk : in std_logic;
-			data_in : in std_logic_vector (N-1 downto 0);
-			data_out : out std_logic_vector (N-1 downto 0));
+	generic (N : natural := 16); --Nombre de bits, a 16 par defaut
+	port (clk : in std_logic; --L'horloge du systeme
+			data_in : in std_logic_vector (N-1 downto 0); --L'entree du registre
+			data_out : out std_logic_vector (N-1 downto 0)); --La sortie du registre
 end regN ;
 
 architecture regNa of regN is
 begin
 
-process (clk,data_in)
+process (clk,data_in) --Le process se reveille
 begin
 
 	if rising_edge(clk) then
